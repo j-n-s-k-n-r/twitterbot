fb87af49d834a81fc0858a5675f3f5cd7c0de998
j-n-s-k-n-r <j-n-s-k-n-r@users.noreply.github.com>
2017-06-30 21:37:46 +0200

    EMail+Wetter
    Add Class for Wetter
    Add unstable Class for Stau

--- src/Verkehrslage.java
+++ src/Verkehrslage.java
@@ -3,59 +3,88 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class Verkehrslage {
 	public String getVerkehrslageAsString() {
-
+		
+		//Unstable Class 
+		/*
+		String inhalt = "";
 		FileReader fr;
 		try {
-			
-			//init Filereader
-			fr = new FileReader("C:\\Users\\jonas-prog\\Desktop\\cred.txt");
-			
-			//init BufferedReader
+
+			// init Filereader
+			fr = new FileReader("C:\\Users\\jonas-prog\\Desktop\\tb\\Inbox");
+
+			// init BufferedReader
 			BufferedReader br = new BufferedReader(fr);
-			
-			//Einlesen EMailadresse aus cred.txt
-			String emailadresse = "";
-			try {
-				emailadresse = br.readLine();
-			} catch (IOException e1) {
-				System.out.println("Emailadresse not found");
-				e1.printStackTrace();
-			}
-			
-			//Einlesen Passwort aus cred.txt
-			String password = "";
-			try {
-				password = br.readLine();
-			} catch (IOException e) {
-				System.out.println("password not found");
-				e.printStackTrace();
-			}
-			
-			//Debugoutput
-			System.out.println(emailadresse);
-			System.out.println(password);
+
+			boolean richtigerTeil = false;
+			boolean inboxEnde5 = false;
+			String inhaltSchleife = "";
+			int i = 0;
 
 			try {
-				br.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				while (inboxEnde5 == false) {
+
+					inhaltSchleife = "";
+					inhaltSchleife = br.readLine();
+					// System.out.println(inhaltSchleife);
+					if (inhaltSchleife.contains("Hier die von Ihnen angeforderten Staumeldungen")) {
+						richtigerTeil = true;
+						System.out.println("wirdTrue");
+					}
+					if (inhaltSchleife.contains(
+							"Alle Suchergebnisse sind ohne Gewähr und erheben keinen Anspruch auf Vollständigkeit und Aktualität.")) {
+						break;
+						// richtigerTeil= false;
+						// System.out.println("getFalse");
+					}
+
+					if (richtigerTeil == true) {
+						System.out.println("D3");
+						inhalt = inhalt + inhaltSchleife; // + br.readLine();
+					}
+					if (inhaltSchleife.isEmpty() == true) {
+						i++;
+					} else {
+						i = 0;
+					}
+
+					if (i == 5) {
+						inboxEnde5 = true;
+					}
+					br.close();
+				}
+
+			} catch (IOException e1) {
+				System.out.println("Error1");
+				e1.printStackTrace();
 			}
 
 		} catch (FileNotFoundException e2) {
-			System.out.println("cred.txt not found");
+			System.out.println("Inbox not found");
 			e2.printStackTrace();
 		}
 
+		String[] abschnitte = inhalt.split("[");
+		String meldungen ="";
+		String[] A42Meldungen;
+		for (int j =0; j<abschnitte.length;j++)
+		{
+			if (abschnitte[j].contains("A42") == true)
+					{
+						A42Meldungen =abschnitte[j].split("A42");
+						for (int k =0; k<A42Meldungen.length;k++)
+						{
+							meldungen = meldungen + A42Meldungen[j];
+						}
+					}
+		}
+		System.out.println(meldungen);
 		
-		
-		
-		//EMailpart
-		// if (xx)
-		// return meldung
-
+		*/
 		return "kein Stau => kein Wunder auch ";
+		
 	}
 }
--- src/Wetter.java
+++ src/Wetter.java
@@ -1,8 +1,98 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Wetter {
-	public String getWetterAsString(){
-		
-		
-		return "Bestes Wetter";
+
+	/*
+	 * Klasse um das Wetter aus dem Emailpostfach (=> hier Thunderbird)
+	 * auszulesen und dann für den Tweet aufzubereiten
+	 */
+	public String getWetterAsString() {
+
+		String inhalt = "";
+		FileReader fr;
+
+		// Auslesen der EMail aus der Textfile in die Thunderbird die Emails
+		// speichert
+		try {
+
+			// init Filereader
+			fr = new FileReader("C:\\Users\\jonas-prog\\Desktop\\wetterbotMail\\Inbox");
+
+			// init BufferedReader
+			BufferedReader br = new BufferedReader(fr);
+
+			boolean richtigerTeil = false;
+			boolean inboxEnde5 = false;
+			String inhaltSchleife = "";
+			int i = 0;
+
+			try {
+				while (inboxEnde5 == false) {
+
+					inhaltSchleife = "";
+					inhaltSchleife = br.readLine();
+					// System.out.println(inhaltSchleife);
+					if (inhaltSchleife.contains("Morgen")) {
+						richtigerTeil = true;
+						// System.out.println("wirdTrue");
+					}
+					if (inhaltSchleife.contains("Die weiteren Aussichten")) {
+						break;
+					}
+
+					if (richtigerTeil == true) {
+						inhalt = inhalt + inhaltSchleife; // + br.readLine();
+					}
+					if (inhaltSchleife.isEmpty() == true) {
+						i++;
+					} else {
+						i = 0;
+					}
+
+					if (i == 5) {
+						inboxEnde5 = true;
+					}
+					br.close();
+				}
+
+			} catch (IOException e1) {
+				System.out.println("Error1");
+				e1.printStackTrace();
+			}
+
+		} catch (FileNotFoundException e2) {
+			System.out.println("Inbox not found");
+			e2.printStackTrace();
+		}
+
+		String ready2Tweet = "";
+		String[] teilStueck = inhalt.split("\\|");
+
+		for (int j = 0; j < teilStueck.length; j++) {
+
+			if (teilStueck[j].contains("Morgen") == true) {
+				/*
+				 * System.out.println(teilStueck[j]);
+				 * System.out.println(teilStueck[j+1]);
+				 * System.out.println(teilStueck[j+2]);
+				 */
+				ready2Tweet = ready2Tweet + "Morgens" + teilStueck[j + 1] + teilStueck[j + 2];
+				ready2Tweet = ready2Tweet + "\n";
+
+			}
+			if (teilStueck[j].contains("Mittag") == true) {
+				ready2Tweet = ready2Tweet + "Mittags" + teilStueck[j + 1] + teilStueck[j + 2];
+				break;
+			}
+
+		}
+		// Ersetzten der Zeichen/Wörter "bis" durch "-" sowie "Grad" durch "°"
+		ready2Tweet = ready2Tweet.replace(" bis ", "-");
+		ready2Tweet = ready2Tweet.replace(" Grad", "°");
+
+		return ready2Tweet;
 	}
 }
